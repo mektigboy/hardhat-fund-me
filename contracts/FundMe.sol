650a0df22cc40b08bfd71b4e8f37c13f8bb09e89
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-22 22:54:54 -0500

    Paste contracts/FundMe.sol, contracts/PriceConverter.sol | Update hardhat.config.js

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,22 +1,78 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
-import "hardhat/console.sol";
+import "./PriceConverter.sol";
 
-contract Greeter {
-    string private greeting;
+error NotOwner();
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+contract FundMe {
+    using PriceConverter for uint256;
+
+    // Keyword constant saves more gas.
+    uint256 public constant MINIMUM_USD = 50 * 1e18; // 1 * 10 ** 18
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFounded;
+
+    // Keyword immutable means you are not going to change the variable.
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner, "Sender is not i_owner.");
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
+        _;
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function fund() public payable {
+        require(
+            msg.value.getConvertionRate() >= MINIMUM_USD,
+            "Did not send enough!"
+        );
+        funders.push(msg.sender);
+        addressToAmountFounded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFounded[funder] = 0;
+        }
+
+        // Reset "funders" array.
+        funders = new address[](0); // New array of addresses with 0 elements.
+
+        // Transfer
+        // Where msg.sender = address.
+        // And payable(msg.sender) = payable address.
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // Send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed.");
+
+        // Call
+        // Returns 2 variables.
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed.");
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal view returns (uint256) {
+        // ABI
+        // Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return uint256(price * 1e10); // 1**10 == 10000000000
+    }
+
+    function getVersion() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+        return priceFeed.version();
+    }
+
+    function getConvertionRate(uint256 ethAmount)
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
