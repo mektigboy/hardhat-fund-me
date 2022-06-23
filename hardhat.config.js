3b3c953530f028d604756a23360d8243683d9eca
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-23 12:17:19 -0500

    Update hardhat.config.js, deploy/01-deploy-fund-me.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
-
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
+require("hardhat-deploy");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     const accounts = await hre.ethers.getSigners();
 
@@ -15,12 +13,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
     solidity: "0.8.15",
     networks: {
@@ -38,5 +30,13 @@ module.exports = {
     },
     etherscan: {
         apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
     },
 };
