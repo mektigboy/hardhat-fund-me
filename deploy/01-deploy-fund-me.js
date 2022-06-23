1a0845b8d379e3fc5dfbe40c9fd531f5ae5f9a85
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-23 15:14:20 -0500

    Update deploy/01-deploy-fund-me.js | Create & update helper-hardhat-config.js

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,7 +1,10 @@
+const { networkConfig } = require("../helper-hardhat-config");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
+    const ethUsdPriceFeed = networkConfig[chainId]["ethUsdPriceFeed"];
     const fundMe = await deploy("FundMe", {
         from: deployer,
         args: [], // Place price feed address.
