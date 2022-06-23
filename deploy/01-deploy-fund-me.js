10091efe9dda598f770342bfb9b948239a7f64b6
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-23 15:03:56 -0500

    Update deploy/01-deploy-fund-me.js

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -2,4 +2,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [], // Place price feed address.
+        log: true,
+    });
 };
