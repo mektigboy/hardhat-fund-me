2b3993cbe1acf761b1a959529b558a1d6f5fed7c
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-23 22:07:24 -0500

    Update deploy/00-deploy-mocks.js, helper-hardhat-config.js

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,5 +1,9 @@
 const { network } = require("hardhat");
-const { developmentChains } = require("../helper-hardhat-config");
+const {
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER,
+} = require("../helper-hardhat-config");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
@@ -8,6 +12,13 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     if (developmentChains.includes(chainId)) {
         log("Local network detected! Deploying mocks...");
-        await deploy("MockV3Aggregator");
+        await deploy("MockV3Aggregator", {
+            contract: "MockV3Aggregator",
+            form: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_ANSWER],
+        });
+        log("Mock deployed!");
+        log("--------------------------------------------------");
     }
 };
