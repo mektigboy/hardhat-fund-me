31a359db7df0934d3db40f6e1efaeedd20cb2244
mektigboy <mektigboy@protonmail.com>
2022-07-28 13:52:02 -0500

    Update deploy/01-deploy-fund-me.js

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -3,6 +3,7 @@ const {
     networkConfig,
     developmentChains,
 } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
@@ -18,11 +19,21 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
     }
 
+    const args = [ethUsdPriceFeedAddress];
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress], // Place price feed address.
+        args: args, // Place price feed address.
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     });
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        // Verify:
+        await verify(fundMe.address, args);
+    }
 
     log("--------------------------------------------------");
 };
