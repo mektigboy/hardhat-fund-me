468b92c6f174f6904ecbab27fa962946e9d02820
mektigboy <mektigboy@protonmail.com>
2022-07-28 12:29:24 -0500

    Update deploy/00-deploy-mocks.js

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -7,6 +7,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId;
+    
     if (chainId == 31337) {
         log("Local network detected! Deploying mocks...")
         await deploy("MockV3Aggregator", {
