c3852fa9b95b526ddc49d5aff1879540733b1c1b
mektigboy <mektigboy@protonmail.com>
2022-07-28 12:29:40 -0500

    Update deploy/01-deploy-fund-me.js

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,14 +1,27 @@
 const { network } = require("hardhat");
-const { networkConfig } = require("../helper-hardhat-config");
+const { networkConfig, developmentChains } = require("../helper-hardhat-config");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
-    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+
+    let ethUsdPriceFeedAddress;
+
+    if (developmentChains.includes(network.name)){
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+        ethUsdPriceFeedAddress = ethUsdAggregator.address;
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    }
+
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [], // Place price feed address.
+        args: [ethUsdPriceFeedAddress], // Place price feed address.
         log: true,
     });
+
+    log("--------------------------------------------------");
 };
+
+module.exports.tags = ["all", "fundme"];
