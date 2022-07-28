ae0e5deac2eecba4907f89954a160a6b77e87e41
mektigboy <mektigboy@protonmail.com>
2022-07-28 13:51:48 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,13 +5,10 @@ require("hardhat-gas-reporter");
 require("solidity-coverage");
 require("hardhat-deploy");
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
-
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-});
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
     solidity: {
@@ -19,17 +16,19 @@ module.exports = {
     },
     defaultNetwork: "hardhat",
     networks: {
-        ropsten: {
-            url: process.env.ROPSTEN_URL || "",
-            accounts:
-                process.env.PRIVATE_KEY !== undefined
-                    ? [process.env.PRIVATE_KEY]
-                    : [],
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
         },
     },
     gasReporter: {
-        enabled: process.env.REPORT_GAS !== undefined,
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
         currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
     etherscan: {
         apiKey: process.env.ETHERSCAN_API_KEY,
