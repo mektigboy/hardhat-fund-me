90626d1e1cd97d9b5a204e2476392e0fc7cbaae1
mektigboy <mektigboy@protonmail.com>
2022-07-29 12:16:58 -0500

    Update

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -57,8 +57,32 @@ describe("FundMe", async function () {
                 deployer
             );
             // Act:
-
+            const transactionResponse = await fundMe.withdraw();
+            const transactionReceipt = await transactionResponse.wait(1);
+            const { gasUsed, effectiveGasPrice } = transactionReceipt;
+            const gasCost = gasUsed.mul(effectiveGasPrice);
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            );
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            );
             // Assert:
+            assert.equal(endingFundMeBalance, 0);
+            assert.equal(
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            );
+        });
+        it("Allows us to withdraw with multiple funders", async function () {
+            // Arrange:
+            const accounts = await ethers.getSigners();
+            for (let i = 1; i < 6; i++) {
+                const fundMeConnectedContract = await fundMe.connect(
+                    accounts[1]
+                );
+                await fundMeConnectedContract.fund({ value: sendValue });
+            }
         });
     });
 });
