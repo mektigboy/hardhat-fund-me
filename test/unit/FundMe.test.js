d813b1e3dc8087c8b6aef6e77ca85d312e821d53
mektigboy <mektigboy@protonmail.com>
2022-07-29 12:19:35 -0500

    Update

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -83,6 +83,29 @@ describe("FundMe", async function () {
                 );
                 await fundMeConnectedContract.fund({ value: sendValue });
             }
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            );
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            );
+            // Act:
+            const transactionResponse = await fundMe.withdraw();
+            const transactionReceipt = await transactionResponse.wait(1);
+            const { gasUsed, effectiveGasPrice } = transactionReceipt;
+            const gasCost = gasUsed.mul(effectiveGasPrice);
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            );
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            );
+            // Assert:
+            assert.equal(endingFundMeBalance, 0);
+            assert.equal(
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            );
         });
     });
 });
